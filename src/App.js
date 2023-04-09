5b5319f92be0cf9f5ce407e8f8268570860b9d2b
Neel028 <neelkoshiya028@gmail.com>
2023-04-09 21:53:49 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import React, { useState } from "react";
 
 function App() {
   let[name,setname]=useState('')
-    let[pass,setpass]=useState('')
+    let[password,setpass]=useState('')
     let[onname,setonname]=useState('')
-    let[onpass,setonpass]=useState('')
+    let[onpassword,setonpassword]=useState('')
     let hand=(e)=>{
         e.preventDefault()
         setname(onname)
-        setpass(onpass)
+        setpass(onpassword)
     }
   return (
     <div className='app'>
       <div className="box">
         <form onSubmit={hand}>
           <input type="text" placeholder="Email" onChange={(e)=>setonname(e.target.value)}/>
-          <input type="password" placeholder="Password" onChange={(e)=>setonpass(e.target.value)}/>
+          <input type="password" placeholder="Password" onChange={(e)=>setonpassword(e.target.value)}/>
           <button type="submit">Submit</button>
         </form>
       </div>
       <h1>Name : {name}</h1>
-      <h1>Password : {pass}</h1>
+      <h1>Password : {password}</h1>
     </div>
   );
 }
