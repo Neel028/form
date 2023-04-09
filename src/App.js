537a0b3031ed1e3aff7b3a85e73c82edbfc6fa2a
Neel028 <neelkoshiya028@gmail.com>
2023-04-09 21:52:18 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  let[name,setname]=useState('')
+    let[pass,setpass]=useState('')
+    let[onname,setonname]=useState('')
+    let[onpass,setonpass]=useState('')
+    let hand=(e)=>{
+        e.preventDefault()
+        setname(onname)
+        setpass(onpass)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className="box">
+        <form onSubmit={hand}>
+          <input type="text" placeholder="Email" onChange={(e)=>setonname(e.target.value)}/>
+          <input type="password" placeholder="Password" onChange={(e)=>setonpass(e.target.value)}/>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <h1>Name : {name}</h1>
+      <h1>Password : {pass}</h1>
     </div>
   );
 }
